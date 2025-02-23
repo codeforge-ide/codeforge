7ab79b8577130a38758ab94e29b8da440e5292f5
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-02-23 09:23:37 +0100

    📝 Update pubspec.

--- lib/theme/dense_text_theme.dart
+++ lib/theme/dense_text_theme.dart
@@ -2,27 +2,24 @@ import 'package:flutter/material.dart';
 
 TextTheme getDenseTextTheme(TextTheme base, {double delta = 2.0}) {
   // Helper to subtract delta if fontSize is non-null.
-  TextStyle? subtractDelta(TextStyle? style) {
-    if (style?.fontSize != null) {
-      return style!.copyWith(fontSize: style.fontSize! - delta);
-    }
-    return style;
+  TextStyle subtractDelta(TextStyle style) {
+    return style.copyWith(fontSize: (style.fontSize ?? 14.0) - delta);
   }
 
   return base.copyWith(
-    displayLarge: subtractDelta(base.displayLarge),
-    displayMedium: subtractDelta(base.displayMedium),
-    displaySmall: subtractDelta(base.displaySmall),
-    headlineLarge: subtractDelta(base.headlineLarge),
-    headlineMedium: subtractDelta(base.headlineMedium),
-    headlineSmall: subtractDelta(base.headlineSmall),
-    titleLarge: subtractDelta(base.titleLarge),
-    titleMedium: subtractDelta(base.titleMedium),
-    titleSmall: subtractDelta(base.titleSmall),
-    bodyLarge: subtractDelta(base.bodyLarge),
-    bodyMedium: subtractDelta(base.bodyMedium),
-    bodySmall: subtractDelta(base.bodySmall),
-    labelLarge: subtractDelta(base.labelLarge),
-    labelSmall: subtractDelta(base.labelSmall),
+    displayLarge: subtractDelta(base.displayLarge!),
+    displayMedium: subtractDelta(base.displayMedium!),
+    displaySmall: subtractDelta(base.displaySmall!),
+    headlineLarge: subtractDelta(base.headlineLarge!),
+    headlineMedium: subtractDelta(base.headlineMedium!),
+    headlineSmall: subtractDelta(base.headlineSmall!),
+    titleLarge: subtractDelta(base.titleLarge!),
+    titleMedium: subtractDelta(base.titleMedium!),
+    titleSmall: subtractDelta(base.titleSmall!),
+    bodyLarge: subtractDelta(base.bodyLarge!),
+    bodyMedium: subtractDelta(base.bodyMedium!),
+    bodySmall: subtractDelta(base.bodySmall!),
+    labelLarge: subtractDelta(base.labelLarge!),
+    labelSmall: subtractDelta(base.labelSmall!),
   );
 }
